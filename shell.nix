44711a4eb8a5ef8d13744142304261940e5d7ec3
Eric S Bullington <eric@ericbullington.com>
2021-02-18 12:53:05 -0500

    Update flake

--- shell.nix
+++ shell.nix
@@ -1,8 +1,8 @@
-{ pkgs ? import <nixpkgs> {} }: 
+{ pkgs, ...  }: 
 pkgs.mkShell {
   # buildInputs is for dependencies you'd need "at run time",
   # were you to to use nix-build not nix-shell and build whatever you were working on
   buildInputs = [
-    pkgs.yq
+    pkgs.jq
   ];
 }
